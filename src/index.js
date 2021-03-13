795617d638e2b44427ab4f01ead07123e6cd3a38
lokesh0177 <70194872+lokesh0177@users.noreply.github.com>
2021-03-13 12:21:39 +0530

    images searching app
    It has asearch bar on the top.
    If you search any images it will show it on your screen. It fetch images from unsplash api.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { lat: null, errorMessage:"" };
+
+	}
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude });
+			},
+			(err) => {
+				this.setState({ errorMessage: err.message});
+			} 
+		);
+	}
+
+	render() {
+		
+			if (this.state.errorMessage && !this.state.lat) {
+				return <div>Error: {this.state.errorMessage}</div>
+			}
+			if (!this.state.errorMessage && this.state.lat) {
+				return <div> <SeasonDisplay lat={this.state.lat} /> </div>
+			}
+
+			return <Spinner message="Please accept location request"/>
+		 
+	}
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
